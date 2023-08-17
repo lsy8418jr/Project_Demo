18035c884f61151caccb640618c12183f974b8f8
lsy84 <lsy84@Scott.guest.dccc.edu>
2023-08-17 16:49:18 -0400

    2023.8.17

---
+++ Project_Demo/src/_2023/_3Q/PowerBall_NumGenarator.java
@@ -0,0 +1,26 @@
+package _2023._3Q;
+
+import java.util.*;
+
+public class PowerBall_NumGenarator {
+	public static void main(String[] args) {
+
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		Random rand = new Random();
+
+		while (numbers.size() < 5) {
+
+			int powerball = rand.nextInt(69) + 1;
+			if (!numbers.contains(powerball)) {
+				numbers.add(powerball);
+			}
+		}
+		Collections.sort(numbers);
+
+		int powerNum = (int) (Math.random() * 26 + 1);
+
+		System.out.println("Your lucky numbers are : \n" + numbers + " + " + powerNum);
+
+	}
+
+}
